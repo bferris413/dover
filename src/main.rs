10a2cee6f83795b0ac1c756408b1b380814cb786
benjamin <bferris413@users.noreply.github.com>
2025-01-29 09:59:12 -0500

    Added support for use statements.
    Uses are flattened, de-aliased, de-duplicated, and collected into
    a HashSet.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use clap::{Parser, Subcommand};
-use std::fs;
-use syn::{File, Item, ItemFn, ItemUse};
+use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about = "Diff OVERview")]
@@ -11,37 +9,6 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let overview = get_overview(&args.file);
-    dbg!(overview);
-}
-
-/// Get an overview of a given Rust file.
-fn get_overview(path: &str) -> Overview {
-    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
-    let file: File = syn::parse_file(&contents).expect("Couldn't parse file {path}");
-
-    let mut imports = Vec::new();
-    let mut functions = Vec::new();
-
-    for item in file.items {
-        match item {
-            Item::Use(item_use @ ItemUse { .. }) => {
-                imports.push(item_use);
-            }
-            Item::Fn(item_fn @ ItemFn { .. }) => {
-                functions.push(item_fn);
-            }
-            _ => {}
-        }
-    }
-
-    let overview = Overview { imports, functions };
-
-    overview
-}
-
-#[derive(Debug)]
-struct Overview {
-    imports: Vec<ItemUse>,
-    functions: Vec<ItemFn>,
+    let overview = dover::get_overview(&args.file).expect("Error getting overview");
+    println!("{overview}");
 }
